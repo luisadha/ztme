33356f2170d54264fa76835ec2ce5a3d172776ce
Luis Adha <40796508+luisadha@users.noreply.github.com>
2025-02-05 00:42:54 +0700

    Add support getopts

--- lib/ztme-toolbox.sh
+++ lib/ztme-toolbox.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
-# Zero Tolerance for Major Errors (ZTME)-toolbox 1.2.2
+# Zero Tolerance for Major Errors (ZTME)-toolbox 1.2.3
 # (c) copyright 2025 by @Luis Adha
 
-banner='[LUIS-TOOLBOX]'
+banner='[ZTME-TOOLBOX]'
 warn='[warning]'
 fail='[failure]'
 succ='[success]'
@@ -11,13 +11,50 @@ succ='[success]'
 #cd $PWD/script/interactive &>/dev/null;
 
 PS3="Your choice: "
-
+function clonePackage() {
+ local args="$1"
+       if timeout 10s ping -c 1 google.com &> /dev/null; then
+         if [ -z ${1:-$REPO_LINK} ]; then
+           echo "$warn : Broken link."
+           exit 1
+         fi
+         REPO_FOLDER=$(basename "${1:-$REPO_LINK}" .git)
+         if git clone "${1:-$REPO_LINK}.git" "$REPO_FOLDER"; then
+         echo "$succ : Repo's was completly cloned, please refresh!"
+       else
+         echo "$fail : Unable to clone the repository, please correct the names. "
+         exit 1
+         fi
+       else
+           echo "$fail : Couldn't connected to internet!, Abort."
+           exit 2
+        fi
+}
 function findMatch() {
   find . -maxdepth 1 -type d -exec sh -c 'for d; do [ -f "$d/${d##*/}.sh" ] && basename "$d/${d##*/}.sh" .sh; done' _ {} +
 }
 function findMatchRecursif() {
   find . -type d -exec sh -c 'for d; do [ -f "$d/${d##*/}.sh" ] && basename "$d/${d##*/}.sh" ".sh"; done' _ {} +
 }
+
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --add-repo)
+            clonePackage "$2"
+            shift 2
+            ;;
+        -h|--help)
+            echo "Help messages"
+            exit 0
+            ;;
+        *)
+            echo "Invalid option: $1"
+            exit 1
+            ;;
+    esac
+done
+
 
 files="$(findMatch)"
 
@@ -26,8 +63,8 @@ sleep 0.8
 clear
 printf %"$(tput cols)"s | tr " " "-"
 #echo "-------------------------------------------"
-echo " Zero Tolerance for Major Errors (ZTME) Toolbox"
-echo " version : v1.2.2"
+echo " Zero Tolerance for Major Errors - Toolbox"
+echo " version : v1.2.3"
 #echo "-------------------------------------------"
 printf %"$(tput cols)"s | tr " " "-"
 echo -e "\nRun script as interactively from repository\nWiki bahasa : <https://telegra.ph/Apa-itu-Ztmexluis-03-02>
@@ -54,23 +91,8 @@ do
     "Add repository")
       echo "$banner : Paste your repository link below :"
       read REPO_LINK
-       if timeout 10s ping -c 1 google.com &> /dev/null; then
-         if [ -z $REPO_LINK ]; then
-           echo "$warn : Broken link."
-           exit 1
-         fi
-         REPO_FOLDER=$(basename "$REPO_LINK" .git)
-         if git clone "$REPO_LINK.git" "$REPO_FOLDER"; then
-         echo "$succ : Repo's was completly cloned, please refresh!"
-       else
-         echo "$fail : Unable to clone the repository, please correct the names. "
-         exit 1
-         fi
-       else
-           echo "$fail : Couldn't connected to internet!, Abort."
-
-           break
-        fi
+    
+      clonePackage $REPO_LINK
       break
       ;;
     *)
